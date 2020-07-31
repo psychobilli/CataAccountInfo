bae0edbe25bcb748615b31899478bb15df4f05a8
psychobilli <richardsw1123@gmail.com>
2020-07-30 21:57:44 -0400

    Updates

---
+++ ORC/SavedVariables/QuestCompletist.lua
@@ -0,0 +1,25 @@
+
+qcSettings = {
+	["QC_M_SHOW_ICONS"] = 1,
+	["QC_L_HIDE_LOWLEVEL"] = 0,
+	["PURGED"] = 99.7,
+	["QC_M_HIDE_INPROGRESS"] = 0,
+	["QC_M_HIDE_PROFESSION"] = 1,
+	["QC_L_HIDE_COMPLETED"] = 0,
+	["QC_M_HIDE_LOWLEVEL"] = 0,
+	["SORT"] = 1,
+	["QC_ML_HIDE_FACTION"] = 1,
+	["QC_L_HIDE_PROFESSION"] = 1,
+	["QC_M_HIDE_SEASONAL"] = 1,
+	["QC_ML_HIDE_RACECLASS"] = 1,
+	["QC_M_HIDE_COMPLETED"] = 0,
+}
+qcWorkingDB = {
+}
+qcWorkingLog = {
+}
+qcCollectedQuests = {
+}
+qcProgressComplete = {
+}
+qcSharedIDs = nil
